8eccee2c5891799efc348dc18b715aab9a8602f3
Miguel <miguelecchavez@gmail.com>
2023-06-08 18:40:31 -0600

    Listou

---
+++ pages/events/[eventId].js
@@ -0,0 +1,45 @@
+//Components
+import ErrorAlert from "@/components/ui/error-alert";
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+
+//Data
+import { getEventById } from "@/dummy-data";
+
+//Tools
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+
+
+function SpecificEventId() {
+
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return (
+      <ErrorAlert>
+        <p>No event Found!</p>
+      </ErrorAlert>
+    );
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        location={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
+}
+
+export default SpecificEventId;
